970a985a010c38ed24b62342f7f1384fa88623ec
Francis Berthe <francis_berthe@hotmail.fr>
2019-09-03 13:37:19 +0200

    Ajout du code pour le menu du site dans le fichier menu.php.

---
+++ menu.php
@@ -0,0 +1,16 @@
+                       <!-- HEADER/ Menu Page Mentions & Page Chapitres -->      
+         
+         
+      <div class="fond-menu">
+          <img class="logo-header" src="css/img/logo-header.png" alt="Logo du blog de Jean Forteroche." />
+          <a href="connexion.php">
+              <div id="bouton-connexion" class="bouton-connexion">
+                 <p class="text-bouton-connexion">connexion</p>
+                 <div class="bouton-connexion-trait"></div>
+              </div>
+          </a>
+          <div class="bouton-accueil">
+             <a href="index.php"><p class="text-bouton-accueil">accueil</p></a>
+             <div class="bouton-accueil-trait"></div>
+          </div>
+       </div>
