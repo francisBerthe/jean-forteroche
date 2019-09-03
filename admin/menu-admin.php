e29411a1b6f48a875c07fb1d7a0421cdcdeef4a8
Francis Berthe <francis_berthe@hotmail.fr>
2019-09-03 14:37:55 +0200

    Ajout du code pour le menu de l'admin dans le fichier menu-admin.php.

---
+++ admin/menu-admin.php
@@ -0,0 +1,14 @@
+                       <!-- MENU ADMIN/ admin.php, post.php & modifier.php -->      
+         
+         
+      <div class="fond-menu">
+          <img class="logo-header" src="css/img/logo-header.png" alt="Logo du blog de Jean Forteroche." />
+          <a href="admin/posts.php">
+              <div class="bouton-oldpost">
+                 <p class="text-bouton-oldpost">Ancien Posts</p>
+              </div>
+          </a>
+          <div class="bouton-newpost">
+             <a href="admin/admin.php"><p class="text-bouton-newpost">Nouveau Post</p></a>
+          </div>
+       </div>
