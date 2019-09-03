0b296856c8098aa6013e597d746c51ab95038c51
Francis Berthe <francis_berthe@hotmail.fr>
2019-09-03 14:40:25 +0200

    Ajout du code pour le bouton UP dans l'admin dans le fichier bouton.php.

---
+++ admin/bouton.php
@@ -0,0 +1,8 @@
+                      <!-- Bouton UP -->
+         
+           
+           
+         <div id="bouton-up" class="bouton-up">
+            <img class="fleche-bouton-up" src="css/img/fleche-bouton.png" alt="fleche bouton-UP">
+            <div class="trait"></div>
+         </div>
