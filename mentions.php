c20c16fdb140e7f7abae9368116750c1b249b0e2
Francis Berthe <francis_berthe@hotmail.fr>
2019-09-03 13:38:19 +0200

    Ajout du code de la page mentions.php dans le fichier mentions.php

---
+++ mentions.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- META Description -->
+  <meta name="identifier-url" content="http://jean-forteroche.francis-berthe.fr/">
+  <meta name="title" content="Blog du livre : Billet Simple pour L'Alaska">
+  <meta name="description" content="Bienvenue sur le blog de Jean Forteroche, découvrez son premier roman 
+                                    et suivez toute l’actu littéraire.">
+  <meta name="author" content=" Berthe Francis">
+  <meta name="publisher" content=" Berthe Francis">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  <!-- OPEN GRAPH Meta -->
+  <meta property="og:title" content="Blog du livre : Billet Simple pour L'Alaska"/>
+  <meta property="og:type" content="Blog" />
+  <meta property="og:url" content="http://jean-forteroche.francis-berthe.fr/" />
+  <meta property="og:image" content="http://example.com/image.jpg" />
+  <meta property="og:description" content="Bienvenue sur le blog de Jean Forteroche, découvrez son premier roman 
+                                           et suivez toute l’actu littéraire.">
+  
+  
+  <title>J.Forteroche - Admin/ Mentions</title>
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="ico/favicon-grand.png"> 
+  <!-- BALISES Link -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- BALISE Font -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700,900" rel="stylesheet"> 
+  <!-- BALISE FontAwesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  
+  <!-- BALISE Style -->
+  <link rel="stylesheet" href="css/mentions%20.css">
+    
+  
+</head>
+<body>
+        
+<!-- HEADER -->
+   
+  <header>
+     <div class="bloc-header">
+       <!-- HEADER/ Menu Page Mentions & Page Chapitres --> 
+         <?php include_once("menu.php"); ?>
+     </div>    
+  </header>       
+         
+                            
+                      <!-- MENTIONS LEGALES -->
+
+  <section>
+    <div class="bloc-texte">
+       <h1 class="h1-mentions">Informations légales</h1>
+       <div class="trait-informations"></div>
+       <br>
+       <h2 class="h2-mentions">1. Présentation du site.</h2>
+       <p class="p-mentions"> En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site <mark>jean-forteroche.francis-berthe.fr</mark>l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
+       <ul class="proprietaire">
+         <li class="li-mentions"><span class="mot-gras">Propriétaire :</span> Berthe Francis – 10, Rue Nelson Mandela – 59113 Seclin</li>
+         <li class="li-mentions"><span class="mot-gras">Créateur :</span> Berthe Francis</li>
+         <li class="li-mentions"><span class="mot-gras">Responsable publication :</span> Berthe Francis – francis_berthe@hotmail.fr</li>
+         <li class="li-mentions"><span class="mot-gras">Webmaster :</span> Berthe Francis – francis_berthe@hotmail.fr</li>
+         <li class="li-mentions"><span class="mot-gras">Hébergeur :</span> 1&amp;1 INTERNET SARL – INTERGEST SA 7 PLACE DE LA GARE  – BP 57200 – SARREGUEMINES</li>
+         <li class="li-mentions"><span class="mot-gras">Crédits :</span> Les mentions légales ont été générées et offertes par Subdelirium Générateur de mentions légales.</li>
+       </ul>
+    
+       <br>
+       <h2 class="h2-mentions">2. Conditions générales d’utilisation du site et des services proposés.</h2>
+        <p class="p-mentions">L’utilisation du site <mark>jean-forteroche.francis-berthe.fr</mark> implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site <mark>jean-forteroche.francis-berthe.fr</mark> sont donc invités à les consulter de manière régulière.<br><br>Ce site est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par Francis Berthe, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.<br><br>Le site <mark>jean-forteroche.francis-berthe.fr</mark> est mis à jour régulièrement par Francis Berthe. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
+       <br>
+       <h2 class="h2-mentions">3. Description des services fournis.</h2>
+       <p class="p-mentions">Le site site <mark>jean-forteroche.francis-berthe.fr</mark> a pour objet de fournir une information liée aux domaines de la lecture et de l’écriture. Francis Berthe s’efforce de fournir sur le site <mark>jean-forteroche.francis-berthe.fr</mark> des informations aussi précises que possible.<br><br> Toutefois, elle ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.<br><br>Tous les informations indiquées sur le site <mark>jean-forteroche.francis-berthe.fr</mark> sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site <mark>jean-forteroche.francis-berthe.fr</mark> ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
+       <br>
+       <h2 class="h2-mentions">4. Limitations contractuelles sur les données techniques.</h2>
+       <p class="p-mentions">Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.</p>
+       <br>
+       <h2 class="h2-mentions">5. Propriété intellectuelle et contrefaçons.</h2> 
+       <p class="p-mentions">Francis Berthe est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes, sons, logiciels.<br><br>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de : Francis Berthe.<br><br>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+       <br>
+       <h2 class="h2-mentions">6. Limitations de responsabilité.</h2>
+       <p class="p-mentions">Francis Berthe ne pourra être tenue responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site <mark>jean-forteroche.francis-berthe.fr</mark>, et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.<br><br>Francis Berthe ne pourra également être tenue responsable des dommages indirects (tels par exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du site <mark>jean-forteroche.francis-berthe.fr</mark>.<br><br>Des espaces interactifs (possibilité de poser des questions dans l’espace contact, possibilités d’ajouter des commentaires aux articles) sont à la disposition des utilisateurs. Francis Berthe se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans ces espaces qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, Francis Berthe se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie…).</p>
+       <br>
+       <h2 class="h2-mentions">7. Gestion des données personnelles.</h2>
+       <p class="p-mentions">En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.<br><br>A l’occasion de l’utilisation du site <mark>jean-forteroche.francis-berthe.fr</mark>, peuvent êtres recueillies : l’URL des liens par l’intermédiaire desquels l’utilisateur a accédé au site <mark>jean-forteroche.francis-berthe.fr</mark>, le fournisseur d’accès de l’utilisateur, l’adresse de protocole Internet (IP) de l’utilisateur.<br><br>En tout état de cause Francis Berthe ne collecte des informations personnelles relatives à l’utilisateur que pour le besoin de certains services proposés par le site <mark>jean-forteroche.francis-berthe.fr</mark>. L’utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu’il procède par lui-même à leur saisie. Il est alors précisé à l’utilisateur du site <mark>jean-forteroche.francis-berthe.fr</mark> l’obligation ou non de fournir ces informations.<br><br>Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en effectuant sa demande écrite et signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.<br><br>Aucune information personnelle de l’utilisateur du site <mark>jean-forteroche.francis-berthe.fr</mark> n’est publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers.<br><br>Le site n’est pas déclaré à la CNIL car il s’agit d’un site personnel ou blog.<br><br>Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
+       <br>
+       <h2 class="h2-mentions">8. Liens hypertextes et cookies.</h2>
+       <p class="p-mentions">Le site <mark>jean-forteroche.francis-berthe.fr</mark> contient un certain nombre de liens hypertextes vers d’autres sites, mis en place avec l’autorisation de Francis Berthe. Cependant, Francis Berthe n’a pas la possibilité de vérifier le contenu des sites ainsi visités, et n’assumera en conséquence aucune responsabilité de ce fait.<br><br>La navigation sur le site <mark>jean-forteroche.francis-berthe.fr</mark> est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur. Un cookie est un fichier de petite taille, qui ne permet pas l’identification de l’utilisateur, mais qui enregistre des informations relatives à la navigation d’un ordinateur sur un site. Les données ainsi obtenues visent à faciliter la navigation ultérieure sur le site, et ont également vocation à permettre diverses mesures de fréquentation.<br><br>Le refus d’installation d’un cookie peut entraîner l’impossibilité d’accéder à certains services. L’utilisateur peut toutefois configurer son ordinateur pour refuser l’installation des cookies :</p>
+       <br>
+       <h2 class="h2-mentions">9. Droit applicable et attribution de juridiction.</h2>
+       <p class="p-mentions">Tout litige en relation avec l’utilisation du site <mark>jean-forteroche.francis-berthe.fr</mark> est soumis au droit français. Il est fait attribution exclusive de juridiction aux tribunaux compétents de Paris.</p>
+       <br>
+       <h2 class="h2-mentions">10. Les principales lois concernées.</h2>
+       <p class="p-mentions">Loi n° 78-17 du 6 janvier 1978, notamment modifiée par la loi n° 2004-801 du 6 août 2004 relative à l’informatique, aux fichiers et aux libertés.<br><br>Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique.</p>
+       <br>
+       <h2 class="h2-mentions">11. Lexique.</h2>
+       <p class="p-mentions"><span class="mot-gras">Utilisateur :</span> Internaute se connectant, utilisant le site susnommé.<br><br><span class="mot-gras">Informations personnelles :</span><i> « les informations qui permettent, sous quelque forme que ce soit, directement ou non, l’identification des personnes physiques auxquelles elles s’appliquent »</i> (article 4 de la loi n° 78-17 du 6 janvier 1978).</p>
+    </div>
+  </section>
+                        
+                  
+                          
+                       <!-- BOUTON -->
+         <?php include_once("bouton.php"); ?>
+                         
+                       <!-- FOOTER -->
+         <?php include_once("footer.php"); ?>
+     
+     
+                        <!-- JQUERY--> 
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
+  </script>  
+  
+                   
+                  <!-- JavaScript/ Bouton -->
+  <script src="bouton.js"></script>
+      <!-- JavaScript/ Connexion -->
+  <script src="main.js"></script>
+
+</body>
+</html>
+  
