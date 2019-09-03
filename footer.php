30a97b3da6230f0a650a0030c9bd49312e5aa02e
Francis Berthe <francis_berthe@hotmail.fr>
2019-09-03 13:40:57 +0200

    Ajout du code pour le footer dans le fichier footer.php.

---
+++ footer.php
@@ -0,0 +1,30 @@
+                   <!-- FOOTER -->
+    
+    <footer>
+          <div class="bandeau-reseaux">
+              <div class="icones-reseaux">
+                  <div class="icones-marge">
+                    <a href="https://plus.google.com/discover"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="https://plus.google.com/discover"><p class="text-google">Google</p></a>
+                  </div>
+                  <div class="icones">
+                    <a href="https://twitter.com/login?lang=fr"><i class="fab fa-twitter"></i></a>
+                    <a href="https://twitter.com/login?lang=fr"><p class="text-twitter">Twitter</p></a>
+                  </div>
+                  <div class="icones">
+                    <a href="https://fr-fr.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://fr-fr.facebook.com/"><p class="text-facebook">Facebook</p></a>
+                  </div>
+                </div>
+            </div>
+                        <!-- Bloc-footer -->
+            <div class="bloc-footer">
+                <img class="logo" src="css/img/logo.png" alt="Logo Jean Forteroche">
+                <a href="mentions.php"><p class="text-bloc-footer">Mentions légales</p></a>
+            </div>
+               
+                          <!-- Rights -->
+            <div class="rights">
+                <p class="text-rights"> All rights reserved. Designed &amp; Developed by <i>Francis Berthe</i></p>
+            </div>
+     </footer>
